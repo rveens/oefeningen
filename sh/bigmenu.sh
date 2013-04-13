f66ceebe9edb9ebe3da1566d1b4f7ef7158e46f2
Rick Veens <rick@rick-HP-625.(none)>
2013-04-13 23:01:32 +0200

    Finished bigmenu shell script

--- sh/bigmenu.sh
+++ sh/bigmenu.sh
@@ -66,15 +66,15 @@ DIVERSEN		90) een bestand editen
 		ls -CF
 		echo 'Geef <RETURN> om verder te gaan' ; read answ
 		;;
-	2) 	echo $CLR ; echo "${BOLD}UNIX-commando: ls -l %{NORMAL}"
+	2) 	echo $CLR ; echo "${BOLD}UNIX-commando: ls -l ${NORMAL}"
 		echo ' PROT-MODE 	EIG	GROEP	LENGTE MOD-TIJD		NAAM'
 		ls -l | pg
 		echo 'Geef <RETURN> om verder te gaan' ; read answ
 		;;
-	3)
-		echo $CLR
-		echo 'Geef de naam van de file waarvan u het soort wilt bepalen: \c'
-		if test -r "$NAAM 2>/dev/null"
+	3)	echo $CLR
+		echo "Geef de naam van de file waarvan u het soort wilt bepalen: \c"
+		read NAAM
+		if test -r "$NAAM" 2>/dev/null
 		then
 			echo "${BOLD}UNIX-commando: file $NAAM ${NORMAL}"
 			echo
@@ -84,8 +84,268 @@ DIVERSEN		90) een bestand editen
 		fi
 		echo 'Geef <RETURN> om verder te gaan' ; read answ
 		;;
-	4)
-		echo 'test1'
+	4) 	echo $CLR
+		echo 'Geef de naam van de file waarvan u het soort wilt bepalen: \c'
+		read NAAM
+		if test -r "$NAAM" 2>/dev/null
+		then
+			echo "${BOLD}UNIX-commando: file $NAAM ${NORMAL}"
+			echo
+			pr -80 -h "$CLR file: $NAAM" $NAAM | pg
+		else
+			echo 'Sorry, file bestaat niet of kan niet door u gelezen worden'
+		fi
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	5)	echo $CLR
+		echo "Geef de naam van de file die u wilt weggooien: \c"
+		read NAAM 
+		if test -f "$NAAM" 2>/dev/null
+		then
+			echo "weet u zeker dat U $NAAM wilt weggooien (j/n): \c"
+			read KEUZE
+			case KEUZE in
+			j*)
+				echo "${BOLD}UNIX-commando: rm $NAAM ${NORMAL}"
+				rm -f $NAAM 2>/dev/null
+				if test -f "$NAAM" 2>/dev/null
+				then
+					echo $NAAM kon niet door u weggegooid worden
+				else
+					echo $NAAM weggegooid
+				fi
+				echo 'Geef <RETURN> om verder te gaan' ; read answ
+				;;
+			*)
+				echo $NAAM niet weggegooid
+				echo 'Geef <RETURN> om verder te gaan' ; read answ
+				;;
+			esac
+		else
+			echo $NAAM bestaat niet
+			echo 'Geef <RETURN> om verder te gaan' ; read answ
+		fi
+		;;
+	6)
+		echo $CLR
+		echo 'Geef de naam van de file waarvan u een kopie wilt maken: \c'
+		read NAAM
+		if test -r "$NAAM" 2>/dev/null
+		then
+			echo 'Geef de naam van het (nieuwe) kopie-bestand: \c'
+			read NAAM2
+			if test -f "$NAAM2" 2>/dev/null
+			then
+				echo "$NAAM2" 2>/dev/null
+				echo Gooi eerst bestand $NAAM2 weg '(optie 4)'
+				echo 'Geef <RETURN> om verder te gaan' ; read answ
+			else
+				echo ${BOLD}UNIX-commando: cp $NAAM $NAAM2 ${NORMAL}
+				if cp $NAAM $NAAM2 2>/dev/null
+				then
+					echo kopie is gemaakt
+				else
+					echo kopie kon niet gemaakt worden
+				fi
+				echo 'Geef <RETURN> om verder te gaan' ; read answ
+			fi
+		else
+			echo $NAAM bestaat niet of kan niet gelezen worden
+			echo U kunt dus ook geen kopie maken
+			echo 'Geef <RETURN> om verder te gaan' ; read answ
+		fi
+		;;
+	7)	echo $CLR
+		echo "Huidige directory: `pwd`"
+		echo 'geldige subdirectories zijn:\n'
+		ls -F | grep '/$' | pr -4 -t -l1 -w80 2>/dev/null
+		echo '\nof ^ om directory naar boven te gaan'
+		echo 'Geef uw keuze: \c'
+		read SUBDIR
+		case $SUBDIR in
+		\^)	SUBDIR=..
+			;;
+		esac
+		if (cd $SUBDIR) 2>/dev/null
+		then
+			echo "${BOLD}UNIX-commando: cd $SUBDIR ${NORMAL}"
+			cd $SUBDIR
+			echo "Huidige directory is nu: `pwd`"
+			echo "Geef <RETURN> om verder te gaan" ; read answ
+		else
+			echo "$SUBDIR bestaat niet of afgesloten voor u"
+			echo 'Geef <RETURN> om verder te gaan' ; read answ
+		fi
+		;;
+	8)	echo $CLR
+		echo 'Geef de naam van de directory die u wilt maken'
+		read NAAM
+		if test -f "$NAAM" -o -d "$NAAM" 2>/dev/null
+		then
+			echo "Sorry, $NAAM is al een bestaande file of directory"
+		else
+			echo "${BOLD}UNIX-commando: mkdir $NAAM ${NORMAL}"
+			if mkdir $NAAM 2>/dev/null
+			then
+				echo "$NAAM als nieuwe directory geinstalleerd"
+			else
+				echo "Sorry, $NAAM kan (hier) niet gemaakt worden"
+			fi
+		fi
+		echo 'Geef <RETURN> onm verder te gaan' ; read answ
+		;;
+	9)	echo $CLR
+		echo 'Geef de naam van de directory die u wilt weggooien: \c'
+		read NAAM
+		if test -d "$NAAM" 2>/dev/null
+		then
+			if [ `ls -a $NAAM | wc -l ` = 2 ]
+			then
+				echo "${BOLD}UNIX-commando: rmdir $NAAM ${NORMAL}"
+				if rmdir $NAAM 2>/dev/null
+				then
+					echo "$NAAM verwijderd"
+				else
+					echo "Sorry, u mag $NAAM niet verwijderen"
+				fi
+			else
+				echo "Waarschuwing: er staan nog bestanden in $NAAM"
+				echo "Weet u zeker dat $NAAM en alles daaronder"
+				echo "verwijderd meot worden (j/n): \c"
+				read KEUZE
+				case KEUZE in
+				j*)
+					echo "${BOLD}UNIX-commando: rm -rf $NAAM ${NORMAL}"
+					if rm -rf $NAAM 2>/dev/null
+					then
+						echo $NAAM weggegooid
+					else
+						$NAAM kon niet door u verwijderd worden
+					fi
+					;;
+				*)	echo $NAAM niet weggegooid
+					;;
+				esac
+			fi
+		else
+			echo "Sorry, $NAAM is geen directory"
+		fi
+		echo 'Geef <RETURN> om verder te gaan' ; read answ 
+		;;
+	11)	echo $CLR
+		echo "${BOLD}UNIX-commando: df ${NORMAL}"
+		echo
+		df
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	12) 	echo $CLR
+		echo 'Geef de naam van de directory: \c'
+		read NAAM
+		if test -d "$NAAM" 2>/dev/null
+		then
+			echo "${BOLD}UNIX-commando: du -s $NAAM ${NORMAL}"
+			echo
+			echo "BLOKKEN	NAAM"
+			if du -s $NAAM
+			then
+				true	#dit is een dummy commando
+			else
+				echo "Waarschuwing: u mag (delen van) $NAAM niet lezen"
+			fi
+		else
+			echo "Sorry, $NAAM is geen directory"
+		fi
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	13)	echo $CLR
+		echo 'Geef de naam van de directory: \c'
+		read NAAM
+		if test -d "$NAAM" 2>/dev/null
+		then
+			echo "${BOLD}UNIX-commando: du $NAAM | sort -nr ${NORMAL}"
+			echo
+			echo "BLOKKEN 	NAAM"
+			du $NAAM | sort -nr | pg
+		else
+			echo "Sorry, $NAAM is geen directory"
+		fi
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	21)	echo $CLR
+		echo "${BOLD}UNIX-commando: who ${NORMAL}"
+		who
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	22) 	echo $CLR
+		echo "${BOLD}UNIX-commando: ps -u ${LOGNAME} ${NORMAL}"
+		echo
+		ps -u ${LOGNAME}
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	23)	echo $CLR
+		echo "${BOLD}UNIX-commando: ps -ef ${NORMAL}"
+		echo
+		ps -ef
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	24)	echo $CLR
+		echo "Sammenvatting info huidige processen:"
+		ps -u ${LOGNAME} | sed -e 's/  */ /g' -e 's/^ //' |
+			cut -d' ' -f1 | tail +2 | pr -3 -t -w80
+		SIG=""
+		while [ "$SIG" != 15 -a "$SIG" != 9 ]
+		do
+			echo "Waarschuwing (w) of stop-signaam (s) sturen: \c"
+			read answers
+			case "$answers" in 
+			w) SIG=15
+				;;
+			s) SIG=9
+				;;
+			*) echo "Foute keuze: geef 'w' of 's'"
+				;;
+			esac
+		done
+		echo "Geef process-nummer(s): \c"
+		read NUMMERS
+		echo 
+		echo "${BOLD}UNIX-commando: kill -$SIG $NUMMERS ${NORMAL}"
+		if kill -$SIG $NUMMERS 2>/dev/null
+		then
+			echo "signaal verstuurd naar $NUMMERS"
+		else
+			echo "u kunt geen signaal sturen naar een of meerdere"
+			echo "processen uit: $NUMMERS"
+		fi
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	90)	echo $CLR
+		echo 'Geef de naam van de file die u wilt editen: \c'
+		read NAAM
+		echo "${BOLD}UNIX-commando: vi $NAAM ${NORMAL}"
+		echo "Zie VI-handleiding over gebruik van editor"
+		sleep 2
+		vi $NAAM
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	91)	echo $CLR
+		echo 'In de UNIX shell kunt u rechtstreeks met de UNIX-commandos werken'
+		sh
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
+		;;
+	99)
+		echo Tot ziens
+		exit
+		;;
+	!*)	COMM=`expr "$KEUZE" : '!\(.*\)'`
+		echo $CLR
+		echo "${BOLD}UNIX-commando: $COMM ${NORMAL}"
+		echo "$COMM"
+		echo 'Geef <RETURN> om verder te gaanA ; read answ'
+		;;
+	*)	echo $CLR
+		echo 'Ongeldige keuze (nummer of !<UNIX-commando> geven!)'
+		echo 'Geef <RETURN> om verder te gaan' ; read answ
 		;;
 	esac
-done
+done 	# einde van de hoofd-loop
