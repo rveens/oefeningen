cd2823faae87de0077e1317fff50b41bcffb64bf
Rick Veens <rickveens92@gmail.com>
2013-07-01 12:31:33 +0200

    Added start of 5-13

--- c/krc/5/oef5-13.c
+++ c/krc/5/oef5-13.c
@@ -6,3 +6,57 @@
  * unreasonable the input or the value of n. Write the program so it makes the 
  * best use of available storage; lines should be stored as in the sorting 
  * program of Section 5.6, not in a two-dimensional array of fixed size. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef MAXLEN
+#define MAXLEN 1000
+#endif
+
+int getinput(char *line);
+int lastlines = 10; // default is 10, as ordered
+
+int main(int argc, char const *argv[])
+{
+	char **p;
+	char **pstart;
+	char *line = malloc(sizeof(char *) * MAXLEN);
+
+	/* read lastline var from command line parameter */
+	while (--argc > 0 && (*++argv)[0] == '-')
+		lastlines = (++*argv)[0] - '0';
+	/* store the input */
+	while (getinput(line)) {
+		p = realloc(p, sizeof(char *) * ((p - pstart) + 1) );
+		*p = malloc(sizeof(char *) * MAXLEN);
+		strcpy(*p, line);
+	}
+	/* print last x lines */
+	char **e = p - lastlines;
+	while (*e) {
+		printf("%s", *e);
+		*e++;
+	}
+
+	/* free memory */
+	while (p > pstart)
+		free(*p--);
+	free(p);
+
+	return 0;
+}
+
+int getinput(char *line)
+{
+	char *s = line;
+
+	while ( (*line = getchar() != EOF) && *line != '\n' )
+		line++;
+	if (*line != '\n')
+		line--;
+	*line = '\0';
+
+	return line - s;
+}
